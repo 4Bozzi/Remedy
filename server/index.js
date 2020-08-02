6e0c982af2fcebffacb2a225ea5ab39081af0771
Andrew Bozzi <4bozzi@gmail.com>
2020-08-02 18:04:30 -0500

    More heroku experiments

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-const config = require('./server/config');
-const { chatToken, videoToken, voiceToken } = require('./server/tokens');
-const Connect = require('./services/Connect.js')
+const config = require('./config');
+const { videoToken } = require('./tokens');
+const Connect = require('../services/Connect.js')
 
+const path = require('path');
 const express = require('express');
 const http = require("http");
 const socketIo = require("socket.io");
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
-
 const port = 3001;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
-
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 const server = http.createServer(app);
 const io = socketIo(server);
