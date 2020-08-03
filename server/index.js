201d27fef5f715ccde726e3ebef3b8f423bc5bd6
Andrew Bozzi <4bozzi@gmail.com>
2020-08-02 20:50:58 -0500

    Try something else

--- server/index.js
+++ server/index.js
@@ -8,64 +8,50 @@ const socketIo = require("socket.io");
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 
-const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3001;
 const SOCKET_PORT = 3001;
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(pino);
+// app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../react-ui/build", "index.html"));
+// });
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+const INDEX = '../react-ui/build/index.html';
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${
-      worker.process.pid
-      } exited: code ${code}, signal ${signal}`
-    );
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const server = http.createServer(app);
+const io = socketIo(server);
+const connect = new Connect();
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("FromAPI", req => {
+    const identity = `User:${Date.now()}`;
+    const room = "TEST";
+    const token = videoToken(identity, room, config);
+
+    JSON.stringify({
+      token: token.toJwt()
+    })
+
+    socket.doctor = connect.connectDoctor(socket);
+    socket.emit("FromAPI", { token: token.toJwt(), roomName: room, username: identity });
   });
-} else {
-  const app = express();
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  app.use(pino);
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../react-ui/build", "index.html"));
+
+  socket.on("disconnect", () => {
+    connect.releaseDoctor(socket.doctor);
+    console.log("Client disconnected");
   });
+});
 
-  const server = http.createServer(app);
-  const io = socketIo(server);
-  const connect = new Connect();
-
-  io.on("connection", (socket) => {
-    console.log("New client connected");
-
-    socket.on("FromAPI", req => {
-      const identity = `User:${Date.now()}`;
-      const room = "TEST";
-      const token = videoToken(identity, room, config);
-
-      JSON.stringify({
-        token: token.toJwt()
-      })
-
-      socket.doctor = connect.connectDoctor(socket);
-      socket.emit("FromAPI", { token: token.toJwt(), roomName: room, username: identity });
-    });
-
-    socket.on("disconnect", () => {
-      connect.releaseDoctor(socket.doctor);
-      console.log("Client disconnected");
-    });
-  });
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-  server.listen(SOCKET_PORT, () => console.log(`Socket server listening on port ${SOCKET_PORT}`));
-}
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Socket server listening on port ${PORT}`));
