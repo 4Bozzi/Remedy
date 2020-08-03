8bebbfa775e066644f94d8a534ba6dd763bde27b
Andrew Bozzi <4bozzi@gmail.com>
2020-08-02 19:30:12 -0500

    More heroku experiments

--- server/index.js
+++ server/index.js
@@ -2,44 +2,69 @@ const config = require('./config');
 const { videoToken } = require('./tokens');
 const Connect = require('../services/Connect.js')
 
-const path = require('path');
 const express = require('express');
 const http = require("http");
 const socketIo = require("socket.io");
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
+const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(pino);
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-const server = http.createServer(app);
-const io = socketIo(server);
-const connect = new Connect();
+import serverRenderer from './middleware/renderer';
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  
-  socket.on("FromAPI", req  => {
-    const identity = `User:${Date.now()}`;
-    const room = "TEST";
-    const token = videoToken(identity, room, config);
-    
-    JSON.stringify({
-      token: token.toJwt()
-    })
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-    socket.doctor = connect.connectDoctor(socket);
-    socket.emit("FromAPI", {token: token.toJwt(), roomName: room, username: identity});
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${
+      worker.process.pid
+      } exited: code ${code}, signal ${signal}`
+    );
   });
+} else {
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(pino);
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-  socket.on("disconnect", () => {
-    connect.releaseDoctor(socket.doctor);
-    console.log("Client disconnected");
+  const server = http.createServer(app);
+  const io = socketIo(server);
+  const connect = new Connect();
+
+  io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on("FromAPI", req => {
+      const identity = `User:${Date.now()}`;
+      const room = "TEST";
+      const token = videoToken(identity, room, config);
+
+      JSON.stringify({
+        token: token.toJwt()
+      })
+
+      socket.doctor = connect.connectDoctor(socket);
+      socket.emit("FromAPI", { token: token.toJwt(), roomName: room, username: identity });
+    });
+
+    socket.on("disconnect", () => {
+      connect.releaseDoctor(socket.doctor);
+      console.log("Client disconnected");
+    });
   });
-});
+}
+
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
