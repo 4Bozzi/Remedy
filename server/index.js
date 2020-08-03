22fdbf0f7b77e77c962943d7f0b4a00c05618e2d
Andrew Bozzi <4bozzi@gmail.com>
2020-08-03 11:50:04 -0500

    Almost 50 deploys to Heroku and it's now working!

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const config = require('./config');
 const { videoToken } = require('./tokens');
 const Connect = require('../services/Connect.js')
-
+const connect = new Connect();
 
 const cors = require('cors');
 const express = require('express');
-//const http = require("http");
-//const socketIo = require("socket.io");
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const path = require('path');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const http = require( "http" ).createServer( app );
@@ -21,7 +18,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
- // Add this
  if (req.method === 'OPTIONS') {
 
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
@@ -39,13 +35,7 @@ app.use(pino);
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-
-//const server = http.createServer(app.listen(PORT));
-
-//const io = socketIo(server);
-// const io = require('socket.io').listen(http);
 io.origins('*:*')
-const connect = new Connect();
 
 io.on("connection", (socket) => {
   console.log("New client connected");
@@ -68,6 +58,3 @@ io.on("connection", (socket) => {
     console.log("Client disconnected");
   });
 });
-
-// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-//server.listen(PORT, () => console.log(`Socket server listening on port ${PORT}`));
