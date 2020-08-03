97525879824453730964379d2a3ef0e282e8d022
Andrew Bozzi <4bozzi@gmail.com>
2020-08-03 09:30:52 -0500

    Try something else

--- react-ui/src/components/Lobby.js
+++ react-ui/src/components/Lobby.js
@@ -3,7 +3,8 @@ import socketIOClient from "socket.io-client";
 import VideoCall from './VideoCall';
 import WaitingScreen from './WaitingScreen';
 
-const ENDPOINT = "http://127.0.0.1:3001/";
+//const ENDPOINT = "http://127.0.0.1:3001/";
+const ENDPOINT = "https://aqueous-brook-27897.herokuapp.com/"
 
 function Lobby() {
   const [roomName, setRoomName] =  useState(null);
