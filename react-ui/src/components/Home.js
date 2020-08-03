7751c934dca2c547830b173ab46dfdac482b676a
Andrew Bozzi <4bozzi@gmail.com>
2020-08-03 12:09:59 -0500

    Fix styles

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -6,6 +6,7 @@ function Home() {
     <div>
       <h2>Home</h2>
       <Link to="/lobby"><span className="btn" >I have an emergency <span role="img" aria-label="screaming face emoji">😱️</span></span></Link>
+      <Link to="/lobby"><span className="btn" >I'm a Doctor <span role="img" aria-label="doctor emoji">👨‍⚕️️</span>!<span role="img" aria-label="screaming face emoji">😱️</span></span></Link>
     </div>
   );
 }
